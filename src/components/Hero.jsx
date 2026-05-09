7b3f2f9e55cb746bafe4ed1406b7093978c1c952
huxlic <oladimejihassan03@gmail.com>
2026-05-09 16:02:29 +0100

    updated images and styles in Hero and Trending components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import heroImg from "../assets/images/cinematic.png";
+import heroImg from "../assets/images/hero.jpg";
 import SearchBar from "./SearchBar";
 
 const Hero = () => {
@@ -7,11 +7,11 @@ const Hero = () => {
       {/* <div className="w-full h-[11vh] bg-[#032541]"></div> */}
       <section
         style={{
-          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(${heroImg})`,
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, .7)), url(${heroImg})`,
         }}
-        className="w-full h-[60vh] bg-cover bg-center] flex flex-col gap-8 px-16 justify-center items-center"
+        className="w-full h-[60vh] bg-cover bg-top flex flex-col gap-8 px-16 justify-center items-center"
       >
-        <h2 className="text-white text-4xl font-bold text-center leading-8 ">
+        <h2 className="text-white text-5xl font-bold text-center leading-10 ">
           Explore the world of entertainment.{" "}
           <span className="text-[1.2rem] block font-semibold">
             Find movies, TV shows, and people worth watching.
