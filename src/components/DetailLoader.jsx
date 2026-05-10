025b197bda1ca037fc14b26e22086ba2a061a601
huxlic <oladimejihassan03@gmail.com>
2026-05-10 22:33:49 +0100

    Refactor DetailLoader component to improve layout and add more skeleton elements

--- src/components/DetailLoader.jsx
+++ src/components/DetailLoader.jsx
@@ -5,21 +5,47 @@ const DetailLoader = () => {
   return (
     <>
       <div className="w-full h-full flex gap-8">
-        <Skeleton className="h-full"
-        // height={420}
+        <Skeleton
+          className="h-full"
           width={300}
           baseColor="#1a1a1a"
           highlightColor="#2d2d2d"
           borderRadius={20}
         />
-        <Skeleton
-          count={2.5}
-          height={16}
-          width={600}
-          borderRadius={20}
-          baseColor="#1a1a1a"
-          highlightColor="#2d2d2d"
-        />
+        <div className=" flex justify-between flex-col">
+          <Skeleton
+            count={2.5}
+            height={16}
+            width={600}
+            borderRadius={20}
+            baseColor="#1a1a1a"
+            highlightColor="#2d2d2d"
+          />
+
+          <div className="">
+            <Skeleton
+              height={16}
+              width={600}
+              borderRadius={20}
+              baseColor="#1a1a1a"
+              highlightColor="#2d2d2d"
+            />
+            <Skeleton
+              height={16}
+              width={600}
+              borderRadius={20}
+              baseColor="#1a1a1a"
+              highlightColor="#2d2d2d"
+            />
+            <Skeleton
+              height={120}
+              width={600}
+              borderRadius={20}
+              baseColor="#1a1a1a"
+              highlightColor="#2d2d2d"
+            />
+          </div>
+        </div>
       </div>
     </>
   );
